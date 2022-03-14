698fa2a73acac381d516096418cf5b6bd64f631c
AoEiuV020 <aoeiuv020@gmail.com>
2022-03-14 20:48:46 +0800

    取消弹窗广告

--- hook/src/main/java/cc/aoeiuv020/hookpicacg/MainHook.java
+++ hook/src/main/java/cc/aoeiuv020/hookpicacg/MainHook.java
@@ -1,7 +1,17 @@
 package cc.aoeiuv020.hookpicacg;
 
+import android.content.Context;
+import android.text.TextUtils;
+import android.view.View;
+
+import org.jetbrains.annotations.Nullable;
+
+import java.lang.reflect.Method;
+
 import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 @SuppressWarnings("RedundantThrows")
@@ -9,5 +19,25 @@ public class MainHook implements IXposedHookLoadPackage {
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
         XposedBridge.log("handleLoadPackage: " + lpparam.processName + ", " + lpparam.processName);
+        Class<?> dialogClass = XposedHelpers.findClassIfExists("com.picacomic.fregata.utils.views.AlertDialogCenter", lpparam.classLoader);
+        if (dialogClass == null) {
+            return;
+        }
+        XposedHelpers.findAndHookMethod(
+                dialogClass,
+                "showAnnouncementAlertDialog",
+                Context.class,
+                String.class,
+                String.class,
+                String.class,
+                String.class,
+                View.OnClickListener.class,
+                new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                        param.setResult(null);
+                    }
+
+                });
     }
 }
