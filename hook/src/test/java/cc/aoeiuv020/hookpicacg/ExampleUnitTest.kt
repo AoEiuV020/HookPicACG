350f534239f2e6e6feee1742e5b046d9727d2133
AoEiuV020 <aoeiuv020@gmail.com>
2022-03-14 20:35:59 +0800

    create project

---
+++ hook/src/test/java/cc/aoeiuv020/hookpicacg/ExampleUnitTest.kt
@@ -0,0 +1,18 @@
+package cc.aoeiuv020.hookpicacg
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        @Suppress("INTEGER_OPERATOR_RESOLVE_WILL_CHANGE")
+        assertEquals(4, 2 + 2)
+    }
+}
