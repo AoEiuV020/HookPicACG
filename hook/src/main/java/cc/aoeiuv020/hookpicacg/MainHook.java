59511f39dc48ce3d2e4613ae78a47b9f2384dddc
AoEiuV020 <aoeiuv020@gmail.com>
2022-03-14 20:35:59 +0800

    create project

---
+++ hook/src/main/java/cc/aoeiuv020/hookpicacg/MainHook.java
@@ -0,0 +1,13 @@
+package cc.aoeiuv020.hookpicacg;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+@SuppressWarnings("RedundantThrows")
+public class MainHook implements IXposedHookLoadPackage {
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        XposedBridge.log("handleLoadPackage: " + lpparam.processName + ", " + lpparam.processName);
+    }
+}
