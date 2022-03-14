5bd4ba5e9ffc7591cff3c922bec5c6e7b43c4a55
AoEiuV020 <aoeiuv020@gmail.com>
2022-03-14 21:31:07 +0800

    移除首页广告

--- hook/src/main/java/cc/aoeiuv020/hookpicacg/MainHook.java
+++ hook/src/main/java/cc/aoeiuv020/hookpicacg/MainHook.java
@@ -1,12 +1,12 @@
 package cc.aoeiuv020.hookpicacg;
 
+import android.app.Activity;
 import android.content.Context;
+import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
-
-import org.jetbrains.annotations.Nullable;
-
-import java.lang.reflect.Method;
+import android.view.ViewGroup;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
@@ -60,6 +60,44 @@ public class MainHook implements IXposedHookLoadPackage {
                     protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
                         XposedBridge.log("beforeHookedMethod: BannerWebview.init(Context)");
                         param.setResult(null);
+                    }
+                });
+        XposedHelpers.findAndHookMethod(
+                "com.picacomic.fregata.activities.MainActivity",
+                lpparam.classLoader,
+                "onCreate",
+                Bundle.class,
+                new XC_MethodHook() {
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        XposedBridge.log("afterHookedMethod: MainActivity.onCreate(Bundle)");
+                        Activity activity = (Activity) param.thisObject;
+                        ViewGroup root = (ViewGroup) ((ViewGroup) (activity.findViewById(android.R.id.content))).getChildAt(0);
+                        for (int i = root.getChildCount() - 1; i >= 0; i--) {
+                            View child = root.getChildAt(i);
+                            if (TextUtils.equals("com.picacomic.fregata.utils.views.BannerWebview", child.getClass().getName())
+                                    || TextUtils.equals("com.picacomic.fregata.utils.views.PopupWebview", child.getClass().getName())) {
+                                root.removeViewAt(i);
+                            }
+                        }
+                    }
+
+                });
+        XposedHelpers.findAndHookMethod(
+                "com.picacomic.fregata.fragments.HomeFragment",
+                lpparam.classLoader,
+                "onCreateView",
+                LayoutInflater.class,
+                ViewGroup.class,
+                Bundle.class,
+                new XC_MethodHook() {
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        XposedBridge.log("afterHookedMethod: HomeFragment.onCreateView");
+                        View viewPager_banner = (View) XposedHelpers.getObjectField(param.thisObject, "viewPager_banner");
+                        ((View) (viewPager_banner.getParent())).setVisibility(View.GONE);
+                        View linearLayout_announcements = (View) XposedHelpers.getObjectField(param.thisObject, "linearLayout_announcements");
+                        linearLayout_announcements.setVisibility(View.GONE);
                     }
                 });
     }
